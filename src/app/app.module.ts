d50eec878f7e96b74e905c0d975cdddcb00548fc
My Name <tmmadi85@gmail.com>
2021-11-12 19:04:34 -0500

    angualar 9 small prog

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
+import { Component3Component } from './component3/component3.component';
+import { Component4Component } from './component4/component4.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Component1Component,
+    Component2Component,
+    Component3Component,
+    Component4Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
